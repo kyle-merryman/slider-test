1c581b8f0ebc5b037669cceae9cd8a8fec4c954c
wintermute <kajmerryman@gmail.com>
2019-03-06 11:44:57 -0800

    Updates

---
+++ precache-manifest.83e8fa4b42052274f12cebb760570adb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "e366885b976bd8a491a70d7b6cc62523",
+    "url": "/slider-test/static/media/activ8.e366885b.svg"
+  },
+  {
+    "revision": "f89f44ef6c9283b6815f",
+    "url": "/slider-test/static/css/main.11737740.chunk.css"
+  },
+  {
+    "revision": "34841c9811ed6487c224",
+    "url": "/slider-test/static/js/runtime~main.34841c98.js"
+  },
+  {
+    "revision": "6f692c866292dcb65dd8b30f5ce9bd87",
+    "url": "/slider-test/static/media/linkedin.6f692c86.svg"
+  },
+  {
+    "revision": "cceed408a4683f494797",
+    "url": "/slider-test/static/js/2.cceed408.chunk.js"
+  },
+  {
+    "revision": "b5dbd91a4a839aeb1eef05c2060c0d17",
+    "url": "/slider-test/static/media/me.b5dbd91a.png"
+  },
+  {
+    "revision": "84405e6374f8fd1c37ddb5aa567adbda",
+    "url": "/slider-test/static/media/casioBeats.84405e63.svg"
+  },
+  {
+    "revision": "1384391014c2017fa9eaa441fef548fa",
+    "url": "/slider-test/static/media/text2speech.13843910.svg"
+  },
+  {
+    "revision": "32debeb604da9a8edbd3fea378a3e311",
+    "url": "/slider-test/static/media/analog.32debeb6.svg"
+  },
+  {
+    "revision": "f89f44ef6c9283b6815f",
+    "url": "/slider-test/static/js/main.f89f44ef.chunk.js"
+  },
+  {
+    "revision": "033d29fa00db3ffbe9961a9542619368",
+    "url": "/slider-test/static/media/canvas.033d29fa.svg"
+  },
+  {
+    "revision": "89909a51d991ebf827fa6a43892bae0c",
+    "url": "/slider-test/static/media/clickyTNG.89909a51.svg"
+  },
+  {
+    "revision": "41dab7d5b8805154ca725efac668dc64",
+    "url": "/slider-test/static/media/raincheck.41dab7d5.svg"
+  },
+  {
+    "revision": "9837164de9599a9859eb89d0040dc2ef",
+    "url": "/slider-test/static/media/webcam.9837164d.svg"
+  },
+  {
+    "revision": "ecd9cee8f899b7d9ef3dbb381b0c2068",
+    "url": "/slider-test/static/media/whack.ecd9cee8.svg"
+  },
+  {
+    "revision": "f9ca6c273492c191d34584606486a693",
+    "url": "/slider-test/static/media/github.f9ca6c27.svg"
+  },
+  {
+    "revision": "106690c90a39a97a82d971397942d7e2",
+    "url": "/slider-test/static/media/email.106690c9.svg"
+  },
+  {
+    "revision": "cceed408a4683f494797",
+    "url": "/slider-test/static/css/2.36e8c0d7.chunk.css"
+  },
+  {
+    "revision": "a9865e876e2a366a5fa4955466f9984e",
+    "url": "/slider-test/index.html"
+  }
+];
