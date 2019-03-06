32eb350999bd8560bce7efc9a0cd4530882da938
wintermute <kajmerryman@gmail.com>
2019-03-05 23:03:03 -0800

    project icons added

--- src/pages/Coding.js
+++ src/pages/Coding.js
@@ -10,6 +10,12 @@ import Icon from "../components/Icon"
 import MusicIco from "../assets/icons/projects/casioBeats.svg";
 import VolumeIco from "../assets/icons/projects/text2speech.svg";
 import ClockIco from "../assets/icons/projects/analog.svg";
+import GlobeIco from "../assets/icons/projects/activ8.svg";
+// import BrushIco from "../assets/icons/projects/canvas.svg";
+// import StarIco from "../assets/icons/projects/clickyTNG.svg";
+// import CloudIco from "../assets/icons/projects/raincheck.svg";
+// import CameraIco from "../assets/icons/projects/webcam.svg";
+// import WhackIco from "../assets/icons/projects/whack.svg";
 /*-----ICONS-----*/
 
  
@@ -44,11 +50,16 @@ class Coding extends Component {
         return (
             <div>
                 <h1></h1>
-
                  <Container>
                     <Icon svg={MusicIco} handleClick={this.handleClick} indx="0"></Icon>
                     <Icon svg={VolumeIco} handleClick={this.handleClick} indx="1"></Icon>
-                    <Icon svg={ClockIco} handleClick={this.handleClick} indx="2"></Icon>                    
+                    <Icon svg={ClockIco} handleClick={this.handleClick} indx="2"></Icon>
+                    <Icon svg={GlobeIco} handleClick={this.handleClick} indx="3"></Icon>  
+                    {/* <Icon svg={BrushIco} handleClick={this.handleClick} indx="4"></Icon>   */}
+                    {/* <Icon svg={StarIco} handleClick={this.handleClick} indx="5"></Icon>   */}
+                    {/* <Icon svg={CloudIco} handleClick={this.handleClick} indx="6"></Icon>   */}
+                    {/* <Icon svg={CameraIco} handleClick={this.handleClick} indx="7"></Icon>   */}
+                    {/* <Icon svg={WhackIco} handleClick={this.handleClick} indx="8"></Icon>                       */}
                 </Container>
 
                 <Modal show={this.state.show} handleClose={this.hideModal} >
